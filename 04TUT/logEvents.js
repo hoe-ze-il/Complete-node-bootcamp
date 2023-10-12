275d40b19393d66de32a50fea1155a86626f77d8
Hoe Ze Il <hoezeil20@gmail.com>
2023-10-12 18:41:54 +0700

    initial commit

---
+++ 04TUT/logEvents.js
@@ -0,0 +1,26 @@
+const { format } = require("date-fns");
+const { v4: uuid } = require("uuid");
+
+const fs = require("fs");
+const fsPromise = require("fs").promises;
+const path = require("path");
+
+const logEvents = async (message) => {
+  const dateTime = `${format(new Date(), "yyyMMdd\tHH:mm:ss")}`;
+  const logItem = `${dateTime}\t${uuid()}\t${message}\n`;
+  console.log(logItem);
+  try {
+    if (!fs.existsSync(path.join(__dirname, "logs"))) {
+      await fsPromise.mkdir(path.join(__dirname, "logs"));
+    }
+    // testing
+    await fsPromise.appendFile(
+      path.join(__dirname, "logs", "eventLog.txt"),
+      logItem
+    );
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+module.exports = logEvents;
